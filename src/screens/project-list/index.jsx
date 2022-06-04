a48af41c3b4648ac5eb5b07b49764d19bfee515a
wuyu <wqiq77@163.com>
2022-06-05 00:06:46 +0800

    fea: 添加工程列表

---
+++ src/screens/project-list/index.jsx
@@ -0,0 +1,45 @@
+import React, { useState, useEffect } from 'react'
+import qs from 'qs'
+import { List } from './list'
+import { SearchPanel } from './search-panel'
+import { cleanObject } from 'utils'
+
+const apiUrl = process.env.REACT_APP_API_URL
+
+export const ProjectListScreen = () => {
+	const [users, setUsers] = useState([])
+	const [param, setParam] = useState({
+		name: '',
+		personId: '',
+	})
+
+	const [list, setList] = useState([])
+	useEffect(() => {
+		fetch(`${apiUrl}/projects?${qs.stringify(cleanObject(param))}`).then(
+			async (resp) => {
+				if (resp.ok) {
+					setList(await resp.json())
+				}
+			}
+		)
+	}, [param])
+
+	useEffect(() => {
+		fetch(`${apiUrl}/users`).then(async (resp) => {
+			if (resp.ok) {
+				setUsers(await resp.json())
+			}
+		})
+	}, [])
+
+	return (
+		<div>
+			<SearchPanel
+				param={param}
+				setParam={setParam}
+				users={users}
+			></SearchPanel>
+			<List list={list} users={users}></List>
+		</div>
+	)
+}
