ee4a3e022b20558ec95cfe31108645b4ce082ccd
wuyu <wqiq77@163.com>
2022-06-05 15:31:04 +0800

    feat: add type

---
+++ src/utils/index.ts
@@ -0,0 +1,50 @@
+import { useEffect, useState } from 'react'
+
+export const isFalsy = (value: any) => (value === 0 ? false : !value)
+
+// 在一个函数里, 改变传入的对象本身是不好的
+export const cleanObject = (object: object) => {
+    // Object.assign({}, object)
+    const result = { ...object }
+    Object.keys(result).forEach((key) => {
+        // @ts-ignore
+        const value = result[key]
+        if (isFalsy(value)) {
+            // @ts-ignore
+            delete result[key]
+        }
+    })
+    return result
+}
+
+export const useMount = (callback: () => void) => {
+    useEffect(() => {
+        callback()
+    }, [])
+}
+
+// const debounce = (func, delay) => {
+//     let timeout
+//     return (...param) => {
+//         if (timeout) {
+//             clearTimeout(timeout)
+//         }
+//         timeout = setTimeout(() => {
+//             func(...param)
+//         }, delay)
+//     }
+// }
+
+export const useDebounce = (value: any, delay?: number) => {
+    const [debouncedValue, setDebouncedValue] = useState(value)
+    useEffect(() => {
+        // 每次在value变化以后, 设置一个定时器
+        const timeout = setTimeout(() => {
+            setDebouncedValue(value)
+        }, delay)
+
+        // 每次在上一次useEffect运行完之后清理
+        return () => clearTimeout(timeout)
+    }, [value, delay])
+    return debouncedValue
+}
