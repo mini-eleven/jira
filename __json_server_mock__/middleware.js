e794e565f94c79deb361b2defdf4238a9c53e3ab
wuyu <wqiq77@163.com>
2022-06-05 16:34:05 +0800

    add jira-server-tool

---
+++ __json_server_mock__/middleware.js
@@ -0,0 +1,18 @@
+module.exports = (req, res, next) => {
+	console.log(req.method)
+	console.log(req.path)
+	console.log(req.body)
+	if (req.method === 'POST' && req.path === '/login') {
+		if (req.body.username === 'admin' && req.body.password === '123') {
+			return res.status(200).json({
+				user: {
+					token: '123',
+				},
+			})
+		}
+	} else {
+		return res.status(400).json({ message: '用户名或密码错误' })
+	}
+
+	next()
+}
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react'
 
-export const isFalsy = (value: any) => (value === 0 ? false : !value)
+export const isFalsy = (value: unknown): boolean => (value === 0 ? false : !value)
 
 // 在一个函数里, 改变传入的对象本身是不好的
 export const cleanObject = (object: object) => {
@@ -35,7 +35,7 @@ export const useMount = (callback: () => void) => {
 //     }
 // }
 
-export const useDebounce = (value: any, delay?: number) => {
+export const useDebounce = <T>(value: T, delay?: number) => {
     const [debouncedValue, setDebouncedValue] = useState(value)
     useEffect(() => {
         // 每次在value变化以后, 设置一个定时器
